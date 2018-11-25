13a8844df10eeb8433ae3d4c58d9120be8ff8efb
Mark Frozen <apexof@gmail.com>
2018-11-25 08:25:45 +0300

    gh-pages routing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
-	"rules": {
-
-	},
+    "extends": "airbnb-base",
+    "rules": {
+        "prefer-arrow-callback": 0,
+        "no-console": 0,
+        'no-plusplus': 'off',
+        "func-names": 0,
+        "no-use-before-define": 0,
+        // "indent": ["error", "tab"],
+        // "no-tabs": 0,
+        "linebreak-style": ["error", "windows"],
+        "strict": 0
+    },
+    "env": {
+        "browser": true,
+        "node": true
+    }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,88 @@
-var gulp          = require('gulp'),
-		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browsersync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-    notify        = require("gulp-notify"),
-    ghPages       = require('gulp-gh-pages');
+'use strict';
 
-gulp.task('browser-sync', function() {
-	browsersync({
-		server: {
-			baseDir: './'
-		},
-		notify: false,
-		// open: false,
-		// tunnel: true,
-		// tunnel: "projectname", //Demonstration page: http://projectname.localtunnel.me
-	})
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const cleancss = require('gulp-clean-css');
+const rename = require('gulp-rename');
+// const remember = require('gulp-remember');
+const autoprefixer = require('gulp-autoprefixer');
+const bSync = require('browser-sync');
+// const imageResize = require('gulp-image-resize');
+const del = require('del');
+const newer = require('gulp-newer');
+const debug = require('gulp-debug');
+const rigger = require('gulp-rigger');
+const ghPages = require('gulp-gh-pages');
+
+gulp.task('serve', function () {
+  bSync({
+    server: './dist',
+    // tunnel: true,
+  });
+  gulp.watch('./dist/**/*.*').on('change', bSync.reload);
 });
 
-gulp.task('styles', function() {
-	return gulp.src('./sass/**/*.sass')
-	.pipe(sass({ outputStyle: 'expand' }).on("error", notify.onError()))
-	.pipe(rename({ suffix: '.min', prefix : '' }))
-	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-	.pipe(gulp.dest('./css'))
-	.pipe(browsersync.reload( {stream: true} ))
+gulp.task('html', function () {
+  return gulp.src('./app/index.html', { since: gulp.lastRun('html') })
+    .pipe(rigger())
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('js', function() {
-	return gulp.src([
-		'./js/*.js', // Always at the end
-		])
-	.pipe(concat('scripts.min.js'))
-	// .pipe(uglify()) // Mifify js (opt.)
-	.pipe(gulp.dest('./js/build'))
-	.pipe(browsersync.reload({ stream: true }))
+gulp.task('assets', function () {
+  return gulp.src('./app/assets/**/*', { since: gulp.lastRun('assets') })
+    .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('sass', function () {
+  return gulp.src('./app/sass/main.sass')
+    // .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer(['last 15 versions']))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(cleancss())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('./dist/css'));
+});
 
- 
-gulp.task('deploy', function() {
+gulp.task('js', function () {
+  return gulp.src('./app/js/*.js')
+    // .pipe(sourcemaps.init())
+    .pipe(babel({ presets: ['@babel/env'] }))
+    .pipe(concat('script.js'))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(uglify())
+    .pipe(rename('script.min.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+// gulp.task('js:libs', function () {
+//   return gulp.src('./app/libs/*.js', { since: gulp.lastRun('js:libs') })
+//     .pipe(newer('./dist/js/libs'))
+//     .pipe(debug({ title: 'js:libs' }))
+//     .pipe(gulp.dest('./dist/js/libs'));
+// });
+
+gulp.task('watch', function () {
+  gulp.watch('./app/sass/**/*.sass', gulp.series('sass'));
+  gulp.watch('./app/**/*.html', gulp.series('html'));
+  gulp.watch('./app/js/*.js', gulp.series('js'));
+});
+
+gulp.task('clean', function () {
+  return del('./dist/*');
+});
+
+gulp.task('deploy', function () {
   return gulp.src('./dist/**/*')
     .pipe(ghPages());
 });
 
-gulp.task('watch', ['styles', 'js', 'browser-sync'], function() {
-	gulp.watch('./sass/**/*.sass', ['styles']);
-	gulp.watch('./js/*.js', ['js']);
-	gulp.watch('./*.html', browsersync.reload)
-});
+gulp.task('build', gulp.series('clean', gulp.parallel('html', 'assets', 'sass', 'js')));
 
-gulp.task('default', ['watch']);
+gulp.task('default', gulp.series('build', gulp.parallel('watch', 'serve')));
